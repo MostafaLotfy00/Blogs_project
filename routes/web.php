34343ea66232cd680f5c96e7583277f7e314861d
MostafaLotfe <mos0112724@gmail.com>
2024-03-15 06:00:43 +0200

    no comment

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-#hello
+
 require __DIR__.'/auth.php';
