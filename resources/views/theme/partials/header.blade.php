dec5915da9c76460d3890a8ab439e4088cf35b1f
Mostafa Lotfy <lotfymostafa800@gmail.com>
2024-03-19 09:55:55 +0200

    Category module completed

--- resources/views/theme/partials/header.blade.php
+++ resources/views/theme/partials/header.blade.php
@@ -1,69 +1,77 @@
- <!--================Header Menu Area =================-->
- <header class="header_area">
-     <div class="main_menu">
-         <nav class="navbar navbar-expand-lg navbar-light">
-             <div class="container box_1620">
-                 <!-- Brand and toggle get grouped for better mobile display -->
-                 <a class="navbar-brand logo_h" href="{{ route('theme.index') }}"><img
-                         src="{{ asset('assets') }}/img/logo.png" alt=""></a>
-                 <button class="navbar-toggler" type="button" data-toggle="collapse"
-                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                     aria-label="Toggle navigation">
-                     <span class="icon-bar"></span>
-                     <span class="icon-bar"></span>
-                     <span class="icon-bar"></span>
-                 </button>
-                 <!-- Collect the nav links, forms, and other content for toggling -->
-                 <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
-                     <ul class="nav navbar-nav menu_nav justify-content-center">
-                         <li class="nav-item @yield('home-active')"><a class="nav-link"
-                                 href="{{ route('theme.index') }}">Home</a></li>
-                         <li class="nav-item @yield('category-active') submenu dropdown">
-                             <a href="#" class="nav-link dropdown-toggle " data-toggle="dropdown" role="button"
-                                 aria-haspopup="true" aria-expanded="false">Categories</a>
-                             <ul class="dropdown-menu">
-                                 <li class="nav-item @yield('category-active') "><a class="nav-link"
-                                         href="{{ route('theme.category') }}">Food</a></li>
-                                 <li class="nav-item @yield('category-active')"><a class="nav-link"
-                                         href="{{ route('theme.category') }}">Bussiness</a></li>
-                                 <li class="nav-item @yield('category-active')"><a class="nav-link"
-                                         href="{{ route('theme.category') }}">Travel</a></li>
-                             </ul>
-                         </li>
-                         <li class="nav-item @yield('contact-active')"><a class="nav-link"
-                                 href="{{ route('theme.contact') }}">Contact</a></li>
-                     </ul>
+@php
+    // $categories = App\Models\Category::take(3)->get();
+    $categories = App\Models\Category::get();
+@endphp
 
-                     <!-- Add new blog -->
-                     <a href="#" class="btn btn-sm btn-primary mr-2">Add New</a>
-                     <!-- End - Add new blog -->
+<!--================Header Menu Area =================-->
+<header class="header_area">
+    <div class="main_menu">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container box_1620">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <a class="navbar-brand logo_h" href="{{ route('theme.index') }}"><img
+                        src="{{ asset('assets') }}/img/logo.png" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+                    <ul class="nav navbar-nav menu_nav justify-content-center">
+                        <li class="nav-item @yield('home-active')"><a class="nav-link"
+                                href="{{ route('theme.index') }}">Home</a></li>
+                        <li class="nav-item @yield('category-active') submenu dropdown">
+                            <a href="#" class="nav-link dropdown-toggle " data-toggle="dropdown" role="button"
+                                aria-haspopup="true" aria-expanded="false">Categories</a>
+                            @if (count($categories) > 0)
 
-                     <ul class="nav navbar-nav navbar-right navbar-social">
-                         @if (!Auth::check())
-                             <a href="{{ route('register') }}" class="btn btn-sm btn-warning">Register / Login</a>
-                         @else
-                             <li class="nav-item submenu dropdown">
-                                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown"
-                                     role="button" aria-haspopup="true"
-                                     aria-expanded="false">{{ Auth::user()->name }}</a>
-                                 <ul class="dropdown-menu">
-                                     <li class="nav-item"><a class="nav-link" href="blog-details.html">My Blogs</a></li>
-                                     <form id="myForm" action="{{ route('logout') }}" method="post">
+                                <ul class="dropdown-menu">
+
+                                    @foreach ($categories as $category)
+                                        <li class="nav-item @yield('category-active') "><a class="nav-link"
+                                                href="{{ route('theme.category') }}">{{ $category->name }}</a></li>
+                                    @endforeach
+                    
+                                </ul>
+                            @endif
+                        </li>
+                        <li class="nav-item @yield('contact-active')"><a class="nav-link"
+                                href="{{ route('theme.contact') }}">Contact</a></li>
+                    </ul>
+
+                    <!-- Add new blog -->
+                    <a href="#" class="btn btn-sm btn-primary mr-2">Add New</a>
+                    <!-- End - Add new blog -->
+
+                    <ul class="nav navbar-nav navbar-right navbar-social">
+                        @if (!Auth::check())
+                            <a href="{{ route('register') }}" class="btn btn-sm btn-warning">Register / Login</a>
+                        @else
+                            <li class="nav-item submenu dropdown">
+                                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
+                                    aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</a>
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item"><a class="nav-link" href="blog-details.html">My Blogs</a></li>
+                                    <form id="myForm" action="{{ route('logout') }}" method="post">
                                         @csrf
-                                       <li class="nav-item">  <a href="#"  class="nav-link" onclick="document.getElementById('myForm').submit(); return false;">log out</a></li>
-                                     </form>
+                                        <li class="nav-item"> <a href="#" class="nav-link"
+                                                onclick="document.getElementById('myForm').submit(); return false;">log
+                                                out</a></li>
+                                    </form>
 
-                                     {{-- <li class="nav-item"><a href="#" class="nav-link"
+                                    {{-- <li class="nav-item"><a href="#" class="nav-link"
                                              onclick="document.getElementById('myForm').submit(); return false;">
                                              log out</a></li> --}}
-                                     {{-- <li class="nav-item"><a class="nav-link" href="blog-details.html">log out</a></li> --}}
-                                 </ul>
-                             </li>
-                         @endif
-                     </ul>
-                 </div>
-             </div>
-         </nav>
-     </div>
- </header>
- <!--================Header Menu Area =================-->
+                                    {{-- <li class="nav-item"><a class="nav-link" href="blog-details.html">log out</a></li> --}}
+                                </ul>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</header>
+<!--================Header Menu Area =================-->
--- resources/views/theme/partials/sidebar.blade.php
+++ resources/views/theme/partials/sidebar.blade.php
@@ -1,3 +1,7 @@
+ @php
+         $categories = App\Models\Category::get();
+ @endphp
+ 
  <!-- Start Blog Post Siddebar -->
  <div class="col-lg-4 sidebar-widgets">
      <div class="widget-wrap">
@@ -23,43 +27,25 @@
                  <button type="submit" class="bbtns d-block mt-20 w-100">Subcribe</button>
          </div>
          </form>
-
+         @if (count($categories) > 0)
+             
          <div class="single-sidebar-widget post-category-widget">
              <h4 class="single-sidebar-widget__title">Catgory</h4>
              <ul class="cat-list mt-20">
-                 <li>
-                     <a href="#" class="d-flex justify-content-between">
-                         <p>Technology</p>
-                         <p>(03)</p>
-                     </a>
-                 </li>
-                 <li>
-                     <a href="#" class="d-flex justify-content-between">
-                         <p>Software</p>
-                         <p>(09)</p>
-                     </a>
-                 </li>
-                 <li>
-                     <a href="#" class="d-flex justify-content-between">
-                         <p>Lifestyle</p>
-                         <p>(12)</p>
-                     </a>
-                 </li>
-                 <li>
-                     <a href="#" class="d-flex justify-content-between">
-                         <p>Shopping</p>
-                         <p>(02)</p>
-                     </a>
-                 </li>
-                 <li>
-                     <a href="#" class="d-flex justify-content-between">
-                         <p>Food</p>
-                         <p>(10)</p>
-                     </a>
-                 </li>
-             </ul>
-         </div>
-
+                @foreach ($categories as $category)
+                    
+                <li>
+                    <a href="#" class="d-flex justify-content-between">
+                        <p>{{ $category->name }}</p>
+                        <p>(03)</p>
+                       </a>
+                   </li>
+                @endforeach
+                
+                </ul>
+            </div>
+            
+            @endif
          <div class="single-sidebar-widget popular-post-widget">
              <h4 class="single-sidebar-widget__title">Recent Post</h4>
              <div class="popular-post-list">
